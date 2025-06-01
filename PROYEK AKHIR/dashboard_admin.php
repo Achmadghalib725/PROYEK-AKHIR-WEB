e863a83d17743b0ae573566292afc973fea60e50
Achmadghalib725 <151291581+Achmadghalib725@users.noreply.github.com>
2025-06-02 01:46:44 +0700

    Delete PROYEK AKHIR directory

--- PROYEK AKHIR/dashboard_admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    header("Location: login.php");
-    exit();
-}
-echo "<h2>Selamat datang, Admin {$_SESSION['username']}!</h2>";
-echo "<a href='logout.php'>Logout</a>";
